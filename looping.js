90b34054542e5798b37d9c5f31af5f7229a5775e
piasmajumdar <piasmajumdar@gmail.com>
2026-02-08 15:42:21 +0530

    looping in arrays

--- looping.js
+++ looping.js
@@ -7,3 +7,33 @@
  * 5. for in --> object loop korar jonno
  * 
 */
+
+const friends = ['Elon', 'Bill', 'Mark', 'Waren'];
+
+for(const friend of friends) {
+    // console.log(friend);
+}
+
+for(let i = 0; i < friends.length; i++) {
+    // console.log(i);
+    // console.log(friends[i]);
+}
+
+
+const numbers = [5, 34,456 ,435 ,456, 3435, 455, 452];
+
+for(let i=0; i<numbers.length; i++) {
+    // console.log(numbers[i]);
+}
+
+let i = 0;
+while (i< friends.length) {
+    // console.log(friends[i]);
+    i++;
+}
+
+let n = 0;
+while (n < numbers.length) {
+    console.log(numbers[n]);
+    n++;
+}
