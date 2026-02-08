e16b593d182e48d010d776952670837ec1fa296f
piasmajumdar <piasmajumdar@gmail.com>
2026-02-09 00:56:15 +0530

    task-7 completed

---
+++ task-7.js
@@ -0,0 +1,29 @@
+/**
+ * Task 7
+Given a 2D array, update the value at second row first item to 99 and print the updated array.
+
+input:
+
+[
+  [1, 2],
+  [3, 4],
+  [5, 6]
+]
+Expected Array:
+
+[
+  [1, 2],
+  [99, 4],
+  [5, 6]
+]
+
+*/
+
+const array1 = [
+  [1, 2],
+  [3, 4],
+  [5, 6]
+];
+
+array1[1][0] = 99;
+console.log(array1);
